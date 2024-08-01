24cacc6104a78acf066e2b6d4e9e0bffec2a8cf0
KimCBNS <desveaux.kim@gmail.com>
2024-08-01 00:22:54 -0300

    update the env variables for render / database

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,32 @@
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
+// require('dotenv').config();
+
+// // const sequelize = process.env.DB_URL
+// //   ? new Sequelize(process.env.DB_URL)
+// //   : new Sequelize(
+// //     process.env.DB_NAME,
+// //     process.env.DB_USER,
+// //     process.env.DB_PASSWORD,
+// //     {
+// //       host: 'localhost',
+// //       dialect: 'postgres',
+// //     }
+// //   );
+
+// module.exports = sequelize;
+
+const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'postgres',
+// Create a new instance of Sequelize using the DATABASE_URL
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
-  );
+  }
+});
 
 module.exports = sequelize;
